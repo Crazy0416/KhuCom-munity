d837761e5be2ecea9fc1f0640f891be56f134c4d
Min-ho Kim <minmin0416@naver.com>
2017-09-30 23:12:10 +0900

    Second commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var tmpLoginJson = require('../config/tmpLoginJson.json');
+var request = require('request');
+var Iconv = require('iconv').Iconv;
+var iconv = new Iconv('EUC-KR', 'UTF-8//TRANSLIT//IGNORE');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', function(req, res){
+  var options = {
+      method: 'POST',
+      url: 'https://klas.khu.ac.kr/user/loginUser.do',
+      headers:{
+          'content-type': 'application/json' },
+      body:JSON.stringify(tmpLoginJson)
+  };
+  console.log(JSON.stringify(tmpLoginJson));
+  var req1 = request(options);
+   req1.on('response', function (res1) {
+       var output = "";
+
+       res1.on('data', function (chunk) {
+           output += iconv.convert(chunk).toString('UTF-8');
+       });
+
+       res1.on('end', function () {
+           console.log(output);
+           res.send(output);
+       });
+   });
+
+})
 
 module.exports = router;
---
+++ test.js
@@ -0,0 +1,160 @@
+ var http = require('http');
+ var request = require("request");
+ var Iconv = require('iconv').Iconv;
+ var iconv = new Iconv('EUC-KR', 'UTF-8//TRANSLIT//IGNORE');
+
+var options = {
+    url: 'https://khuis.khu.ac.kr/java/servlet/khu.cosy.login.loginCheckAction',
+    port: 80,
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body:"user_id=2013104056&password=KIMho0715!@&RequestData="
+};
+
+
+var req = request(options);
+
+var cookie = {};
+
+function setCookie(cookies) {
+    for (var i in cookies) {
+        var c = cookies[i];
+        var tokens = c.split(";");
+        for (var j in tokens) {
+            var t = tokens[j].trim();
+            var tt = t.split("=");
+            cookie[tt[0]] = tt[1];
+        }
+    }
+}
+function getCookie() {
+    var str = "";
+    for (var i in cookie) {
+        str += i + "=" + cookie[i] + "; ";
+    }
+    return str;
+}
+
+function getCookieConsole(){
+    var str = "";
+    for (var i in cookie) {
+        str += i + "=" + cookie[i] + "; " + '\n';
+    }
+    console.log(str);
+}
+
+req.on('response', function(res) {
+    res.setEncoding('utf8');
+
+    res.on('end', function () {
+        setCookie(res.headers["set-cookie"]);
+        console.log(res.headers);
+        options = {
+            url: 'https://khuis.khu.ac.kr/java/servlet/controllerCosy?' +
+            'action=17&WID=hsip1004&Pkg=JSP&URL=' +
+            'JSP./jsp/hssu/infospace/SugangSearchPrintList.jsp[(QUES)]auto=off',
+            port: 80,
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Cookie' : getCookie()
+            }
+        };
+
+        var req2 = request(options);
+        req2.on('response', function (res) {
+            res.setEncoding('utf8');
+            res.on('end',function () {
+                console.log(res.headers);
+                setCookie(res.headers["set-cookie"]);
+
+                options = {
+                    url: 'https://khuis.khu.ac.kr/java/servlet/controllerHssu',
+                    port: 80,
+                    method: 'POST',
+                    headers: {
+                        'Host' : "khuis.khu.ac.kr",
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Cookie' : getCookie()
+                    },
+                    body:"action=655&auto=on&lectYear=2017&lectTerm=20"
+                };
+
+                var req3 = request(options);
+                req3.on('response', function (res) {
+                    var output = "";
+                    res.setEncoding('utf8');
+
+                    res.on('data', function (chunk) {
+                        output += chunk;
+                    });
+
+                    res.on('end', function () {
+
+                        setCookie(res.headers["set-cookie"]);
+                        console.log(res.headers);
+                        console.log(output);
+                        console.log("cookie : ");
+                        getCookieConsole();
+                    });
+
+
+                });
+
+            });
+        })
+
+    });
+
+}).on('error', function(err) {
+    console.log(err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//  var options = {
+//     url: 'https://khuis.khu.ac.kr/java/servlet/khu.cosy.login.loginCheckAction',
+//     port: 80,
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//      encoding: null,
+//     body:"user_id=2013104056&password=KIMho0715!@&RequestData="
+// };
+//
+// var req = request(options);
+// req.on('response', function (res) {
+//     var output = new Buffer([]);
+//
+//     res.on('data', function (chunk) {
+//         output = Buffer.concat([output, chunk]);
+//     });
+//
+//     res.on('end', function () {
+//
+//
+//         console.log(iconv.convert(output).toString());
+//         console.log(res.headers);
+//     });
+//
+//
+// });
