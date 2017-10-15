8e51df3a566ed99d7af442b9b7e2a244c789d8a2
kyungee <kyungee.kim@gmail.com>
2017-10-16 00:09:45 +0900

    [모듈추가] ejs-locals

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('ejs', engine);
 
 app.use('/', index);
 app.use('/users', users);
