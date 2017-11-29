3ac26c234976ee228fe63519d9802412708a264d
Min-ho Kim <minmin0416@naver.com>
2017-11-29 10:31:57 +0900

    GET student notice page from mysql data

--- routes/notice.js
+++ routes/notice.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
+var mysql = require('mysql-promise');
 
 // show server time
 router.use(function(req, res, next){
@@ -13,8 +14,38 @@ router.get('/', function(req, res, next) {
   res.render('notice');
 });
 
-router.get('/stu', function(req, res, next) {
-  res.render('stu-notice');
+router.get('/stu/:page_num', function(req, res, next) {
+  var brdPostCnt;          // 보드 post의 총 개수
+  var page_cnt;         // 페이지 총 개수
+  var page_num = req.params.page_num;   // view 원하는 페이지 number\
+  var data = {};
+  mysql.query('SELECT brd_count FROM Board')
+      .spread(function(rows){
+          brdPostCnt = rows[0]['brd_count'];
+          page_cnt = Math.ceil(brdPostCnt / 10);   // 올림
+          // 페이지마다 어디까지 보여줄 지 확인
+          return mysql.query('SELECT * FROM Post WHERE Board_brd_id=? ORDER BY post_num DESC LIMIT ?, ?', [1,(page_num-1)*10, 10]);
+      })
+      .spread(function(rows){
+          data.data = {};
+          rows.forEach(function(ele, index){
+              var post = {
+                  "post_title": ele.post_title,
+                  "post_num" : ele.post_num,
+                  "post_nickname" : ele.post_nickname,
+                  "post_username" : ele.post_username,
+                  "post_hit" : ele.post_hit,
+                  "post_comment_count" : ele.post_comment_count,
+
+              };
+              data.data['d'+index] = post;
+          })
+      })
+      .then(function(){
+          data.page_cnt = page_cnt;
+          data.page_num = page_num;
+          res.render('stu-notice', data);
+      })
 });
 
 
