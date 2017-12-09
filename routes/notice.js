1b6479bce94f35efac7fdb6d7ae56d1a1515333c
Min-ho Kim <minmin0416@naver.com>
2017-12-09 11:47:19 +0900

    Transaction INSERT Post data and UPDATE Board brd_count

--- routes/notice.js
+++ routes/notice.js
@@ -2,13 +2,13 @@ var http = require('http');
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var mysql = require('mysql-promise')();
-
+var mysqlConnection = require('mysql');
 
 // config file
 var mysql_config = require('../config/db_config.json');
 // mysql config tab
-mysql.configure(mysql_config);
+var mysql = mysqlConnection.createConnection(mysql_config);
+
 
 // show server time
 router.use(function(req, res, next){
@@ -44,43 +44,64 @@ router.get('/stu/write', function(req, res, next) {
   res.render('stu-notice_write', sendData);
 });
 
-function objToString (obj) {
-    var str = '';
-    for (var p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            str += p + '::' + obj[p] + '\n';
-        }
-    }
-    return str;
-}
+router.post('/stu/write', function(req, res, next) {
+    console.log(0);
 
-router.post('/stu/write', function(req, res, next) {1
+    console.log(1);
+    mysql.query('SELECT brd_count FROM Board WHERE brd_id=?', 1, function (err, result, fields) {
+        console.log('POST /stu/write SELECT ok : ' + JSON.stringify(result));
+        var post_num = result[0]['brd_count'] + 1;
+        console.log('post_num : ' + post_num);
+        var post_title = "임의의 글" + post_num;
+        var post_mem_id = req.session.mem_id;
+        var post_content = req.body['msg'];
+        var post_username = req.session.username;
+        var post_nickname = req.session.nickname;
+        var registerTime = moment().format('YYYY/MM/DD HH:mm:ss');
+        mysql.beginTransaction(function (err) {
+            if(err) {throw err;}
+            mysql.query('INSERT INTO Post (Board_brd_id, Member_mem_id, post_num, post_title, post_content, post_username, post_nickname, post_register_datetime, post_hit, post_comment_count, post_file, post_image, post_blame, post_del) '
+                + 'Values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[1, post_mem_id, post_num, post_title, post_content, post_username, post_nickname, registerTime, 0, 0, null, null, null, null]
+                , function(err, result, fields){
+                    if (err) {
+                        return mysql.rollback(function() {
+                            throw err;
+                        });
+                    }
+                    console.log('POST /stu/write INSERT ok : ' + JSON.stringify(result));
+                    mysql.query('UPDATE Board SET brd_count=brd_count+1 WHERE brd_id=?', 1, function (err1, result1, fields1) {
+                        if (err1) {
+                            return mysql.rollback(function() {
+                                throw err1;
+                            });
+                        }
+                        console.log('POST /stu/write UPDATE ok : ' + result1 + '\n' + fields1);
 
-    mysql.query('SELECT brd_count FROM Board WHERE brd_id=?', 1)    // 학생회 공지사항 Board 정보 가져옴
-        .then(function(rows){
-            var post_num = rows[0][0]['brd_count'] + 1;
-            var post_title = "임의의 글" + post_num;
-            var post_mem_id = req.session.mem_id;
-            var post_content = req.body['msg'];
-            var post_username = req.session.username;
-            var post_nickname = req.session.nickname;
-            var registerTime = moment().format('YYYY/MM/DD HH:mm:ss');
-            return mysql.query('INSERT INTO Post (Board_brd_id, Member_mem_id, post_num, post_title, post_content, post_username, post_nickname, post_register_datetime, post_hit, post_comment_count, post_file, post_image, post_blame, post_del) '
-            + 'Values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)',[1, post_mem_id, post_num, post_title, post_content, post_username, post_nickname, registerTime, 0, 0, null, null, null, null]);
+                        mysql.commit(function (err2) {
+                            if(err2){
+                                return mysql.rollback(function () {
+                                    throw err2;
+                                })
+                            }
+                            console.log('POST /stu/write TRANSACTION SUCCESS!!');
+                            res.send({
+                                'SUCCESS' : 1,
+                                'url' : req.protocol + '://' + req.get('host') + '/notice/stu'
+                            })
+                        })
+                    })
+                });
         })
-        .then(function (rows) {
-            console.log("Stu Write INSERT Success!! : " + JSON.stringify(rows));
-            res.send({
-                'SUCCESS' : 1,
-                'url' : req.protocol + '://' + req.get('host') + '/notice/stu'
-            })
-        })
+    })  // TODO : 에러 핸들링
+        /*
         .catch(function (err) {
+            console.log('POST /stu/write : ' + err);
             res.send({
                 'SUCCESS' : 0,
                 'url' : req.protocol + '://' + req.get('host') + '/notice/stu'
             })
         })
+        */
 
 });
 
