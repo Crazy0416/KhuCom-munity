8a4396215b7f6a5809f83b233401ef37a882c87a
Min-ho Kim <minmin0416@naver.com>
2017-12-16 22:51:59 +0900

    [Add] Add reduceWord event

--- modules/reduceSearchWord.js
+++ modules/reduceSearchWord.js
@@ -4,11 +4,19 @@ var searchWordCount = require('../modules/seachWordCount');
 
 // EventEmitter 객체 생성
 var eventEmitter = new events.EventEmitter();
-/*
-eventEmitter.on('reduceWord', function(){
-    for(key in searchWordCount.listObj){
 
+// TODO : Top k 20개 정도로 제한
+eventEmitter.on('reduceWord', function(callback){
+    var reduceList = [];
+    var listObjCnt = searchWordCount.listObj.length;
+    for(key in searchWordCount.listObj){
+        var dataObj = {
+            'name' : key,
+            'value' : searchWordCount.listObj[key]
+        }
+        reduceList.push(dataObj);
+        if(searchWordCount.listObj[listObjCnt-1] === key) {
+            callback(reduceList);
+        }
     }
 })
-*/
-
