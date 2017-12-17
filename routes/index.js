4f5edadc469b6321e1b5b8bd0cd069ea764335de
Min-ho Kim <minmin0416@naver.com>
2017-12-17 18:19:39 +0900

    [Add] level 4 View circle register list

--- routes/index.js
+++ routes/index.js
@@ -62,16 +62,30 @@ router.get('/', function(req, res, next) {
   else
       sendData.mem_username = null;
 
-  if(req.query.alertMessage){
-      console.log("alert!! in page : " + req.query.alertMessage);
-      sendData.alertMessage = req.query.alertMessage;
-  }
+    if(req.session.level === 4){
+        sendData.circleRegisterList = [];
+        mysql.query('SELECT Circle_Regi_Holder.Member_mem_id, Member.mem_username, Circle_Regi_Holder.crh_register FROM Circle_Regi_Holder INNER JOIN Member WHERE Member.mem_id=Circle_Regi_Holder.Member_mem_id AND Circle_Regi_Holder.Circle_circle_id=?', req.session.circle, function(err, result, fields){
+            console.log('GET / SELECT : ' + JSON.stringify(result));
+            sendData.circleRegisterList = result;
+            if(req.query.alertMessage){
+                console.log("alert!! in page : " + req.query.alertMessage);
+                sendData.alertMessage = req.query.alertMessage;
+            }
+            res.render('index', sendData);
+        })
+    }else {
+        if(req.query.alertMessage){
+            console.log("alert!! in page : " + req.query.alertMessage);
+            sendData.alertMessage = req.query.alertMessage;
+        }
 
-  res.render('index', sendData);
+        res.render('index', sendData);
+    }
 });
 
 router.get('/base', function(req, res, next) {
-  res.render('base', { title: 'Express' });
+
+    res.render('base', { title: 'Express' });
 });
 
 module.exports = router;
