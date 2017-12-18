8718912c8e3deea002ce942fab40f93870e97bda
Min-ho Kim <minmin0416@naver.com>
2017-12-19 02:44:22 +0900

    [Fix] Fix use wrong syntax

--- routes/board.js
+++ routes/board.js
@@ -72,24 +72,16 @@ router.post('/photo', upload.any(), function(req, res, next){
         mysql.query('SELECT brd_count FROM Board WHERE brd_id=?', 3, function (err, result, fields) {
             console.log('POST /stu/write SELECT ok : ' + JSON.stringify(result));
             var post_num = result[0]['brd_count'] + 1;
-            console.log('post_num : ' + post_num);
             var post_title = req.body.title;
             var post_mem_id = req.session.mem_id;
-            console.log('post_mem_id : '+ post_mem_id)
             var post_content = null;
             var post_username = req.session.username;
-            console.log('post_username : '+ post_username)
             var post_nickname = req.session.nickname;
-            console.log('post_nickname : '+ post_nickname)
             var registerTime = moment().format('YYYY/MM/DD HH:mm:ss');
             var f_originname = req.files[0].originalname;
-            console.log('f_originname : '+ f_originname)
             var f_name = req.files[0].filename;
-            console.log('f_name : '+ f_name)
             var f_size = req.files[0].size;
-            console.log('f_size : '+ f_size)
             var f_type = req.files[0].mimetype;
-            console.log('f_type : '+ f_type)
             var f_is_img = 1;
             var width = 0;
             var height = 0;
@@ -104,7 +96,7 @@ router.post('/photo', upload.any(), function(req, res, next){
                             });
                         }
                         console.log('POST /board/photo INSERT ok : ' + JSON.stringify(result));
-                        mysql.query('UPDATE Board SET brd_count=brd_count+1 WHERE brd_id=?', 1, function (err1, result1, fields1) {
+                        mysql.query('UPDATE Board SET brd_count=brd_count+1 WHERE brd_id=?', 3, function (err1, result1, fields1) {
                             if (err1) {
                                 return mysql.rollback(function() {
                                     throw err1;
